12011077506b491735d7d622d3ae997080398d3a
naresh546nk <91820009+naresh546nk@users.noreply.github.com>
2022-08-15 13:35:25 +0530

    added change to trigger the build

--- src/main/java/com/pipeline/PipelineDemoApplication.java
+++ src/main/java/com/pipeline/PipelineDemoApplication.java
@@ -1,12 +1,14 @@
 package com.pipeline;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j
 public class PipelineDemoApplication {
-
 	public static void main(String[] args) {
+		System.out.println("Application started successfull .................");
 		SpringApplication.run(PipelineDemoApplication.class, args);
 	}
 
